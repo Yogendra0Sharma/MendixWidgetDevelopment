a80e0406cc8255868da67c91b97f7e447371bf99
Yogendra Sharma <eh.yogendra@gmail.com>
2020-06-17 17:32:24 +0530

    Objects example

---
+++ Day1/1.Basics Of Javascript/ClassAndObjects/Objects.js
@@ -0,0 +1,15 @@
+
+let person = {
+    name:"yogendra",
+    age:28
+}
+// check type of person
+console.log(typeof person);
+// get person name property
+console.log(person.name);
+
+// add new property to person object
+person["org"] = "intelizign";
+// print person object
+console.log(person);
+
