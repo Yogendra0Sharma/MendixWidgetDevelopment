85c6344a93252ad76f62013c59eabc69db37610d
Yogendra Sharma <eh.yogendra@gmail.com>
2020-06-23 21:04:11 +0530

    Day 2 Code

--- Day2/TaskWidget/myapp/widget/TaskWidget.js
+++ Day2/TaskWidget/myapp/widget/TaskWidget.js
@@ -1,10 +1,9 @@
 //TaskWidget/widget/TaskWidget.js
-define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin",
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin","dojo/on","dojo/dom",
         "dojo/text!./templates/TasksWidget.html"
     ],
-    function (declare,_WidgetBase, _TemplatedMixin,template) {
+    function (declare,_WidgetBase, _TemplatedMixin,dom,event,template) {
         return declare([_WidgetBase, _TemplatedMixin], {
-
             // Our template - important to load & parse template
             templateString: template,
         });
